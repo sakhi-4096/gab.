dcbcd0981a3cee0db7ae5758da6fe27318724609
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-22 15:20:48 +0200

    Fix spelling errors.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtfotms.validators import DataRequired
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BoleanField('Remember Me')
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
