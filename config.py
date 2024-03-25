8767d4ed71a3e7449ec46431d88909ad617e1d04
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-25 22:04:45 +0200

    Add Flask-SQLAlchemy configuration.

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
