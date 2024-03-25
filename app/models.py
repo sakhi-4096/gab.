4b234a5c2d2ec406bd74a00b4f45c132acbc6a21
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-25 23:21:38 +0200

    Fix 'User' models relationship to 'Post' model.

--- app/models.py
+++ app/models.py
@@ -12,6 +12,9 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
