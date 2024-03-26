70865181f91a588450829897a9991a8ac95e9dc4
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-26 10:49:17 +0200

    Do not pass user to template anymore.

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
