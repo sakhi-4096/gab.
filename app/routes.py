24152b6edab3961dae97d97ec9f9ef23df86f3da
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-26 18:46:37 +0200

    Record time of last visit.

--- app/routes.py
+++ app/routes.py
@@ -71,3 +71,9 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
