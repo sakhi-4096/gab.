7475ff7e97fef08b68a58e7d6d212dff6b0bda0a
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-26 09:48:01 +0200

    Flask-Login user mixin class.

--- app/models.py
+++ app/models.py
@@ -4,7 +4,9 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
-class User(db.Model):
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
