fa1200efc4d23088a4180deef0a381049751a6a6
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2024-03-27 09:57:59 +0200

    Fix circular import.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from app import routes, models, errors
 import logging
 from logging.handlers import SMTPHandler
 
